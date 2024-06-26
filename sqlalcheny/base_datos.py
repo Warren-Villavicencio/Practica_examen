3fd9915c665c936ac3375af2e9df7aca56177ca3
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-06-26 13:25:57 -0500

    carpeta de tarea

--- sqlalcheny/base_datos.py
+++ sqlalcheny/base_datos.py
@@ -7,3 +7,39 @@ from sqlalchemy import create_engine
 # sqlite
 engine = create_engine('sqlite:///ejemplo001.db')
 
+# se crea la clase llamada Base que permite definir las clases bajo las
+
+# características de SQLAlchemy
+
+Base = declarative_base()
+
+
+# Se crea la una entidad llamada Autor, que hereda
+
+# de Base
+
+class Autor(Base):
+
+ __tablename__ = 'autor' # El nombre de la entidad en sqlite
+
+# Se definen los atributos
+
+id = Column(Integer, primary_key=True) # este atributo es entero y
+
+# se lo considera como llave
+
+# primaria
+
+nombre = Column(String) # atributo de tipo String
+
+apellido = Column(String)
+
+nacionalidad = Column(String)
+
+edad = Column(Integer)
+
+# Sentencia que permite crear o migrar las clases en Python al
+
+# gestor de base de datos, expresado en el engine.
+
+Base.metadata.create_all(engine)
