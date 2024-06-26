efff1aaba56a1051d5642b728f57807ef549cb06
Warren Antonio Villavicencio Merino <wavillavicencio@utpl.edu.ec>
2024-06-26 05:01:17 -0500

    6  envio

--- parctica/creacion_entidades.py
+++ parctica/creacion_entidades.py
@@ -1,6 +1,10 @@
 from base_datos import conn
+
 cursor = conn.cursor()
+
 cadena_sql = 'CREATE TABLE Autor (nombre TEXT, apellido TEXT, cedula TEXT, \
  edad INTEGER)'
+ 
 cursor.execute(cadena_sql)
+
 cursor.close()
